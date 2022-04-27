a92a2d8764b5021c34a49880e9c5f99f61edb48b
FredDietrich <frederico.machry@gmail.com>
2022-04-27 20:18:18 -0300

    Co-authored-by: Gabriel Oliveira Goulart <gahh2001@users.noreply.github.com>

--- techConnectBackend/techConnect/urls.py
+++ techConnectBackend/techConnect/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('newcode', views.newcode, name='newcode'),
     path('logout', views.logout, name='logout'),
-    path('user', views.user, name='user')
+    path('user', views.user, name='user'),
+    #path('bloodpressure', views.bloodpressure, name='bloodpressure'),
+    #path('glucose', views.glucose, name='glucose'),
+    path('weight', views.weight, name='weight'),
+    #path('vaccines', views.vaccines, name='vaccines')
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
