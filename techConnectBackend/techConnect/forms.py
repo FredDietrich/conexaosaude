7fbda3493f4b0acee93d813f616de066c5c1df7e
FredDietrich <frederico.machry@gmail.com>
2022-04-27 20:19:32 -0300

    Cria telas de cadastro e usuario

--- techConnectBackend/techConnect/forms.py
+++ techConnectBackend/techConnect/forms.py
@@ -8,7 +8,9 @@ class RegisterForm(forms.Form):
     date = forms.DateField(widget=forms.SelectDateWidget(
         years=range(datetime.date.today().year, 1900, -1)
     ), label="Data de nascimento")
-    email = forms.EmailField(max_length=15, widget=forms.EmailInput(attrs={'onkeyup': 'validateEmail(this.value)'}), label="E-mail")
+    email = forms.EmailField(max_length=150, widget=forms.EmailInput(attrs={'onkeyup': 'validateEmail(this.value)'}), label="E-mail")
+    phone = forms.CharField(max_length=15, label="Celular")
+    cpf = forms.CharField(max_length=15, label='CPF')
     password = forms.CharField(widget=forms.PasswordInput, max_length=20, label="Senha")
 
 class LoginForm(forms.Form):
@@ -16,7 +18,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput, max_length=20, label="Senha")
     
 class TwoFactor(forms.Form):
-    code = forms.DecimalField(max_value=9999, min_value=1000)
+    code = forms.DecimalField(max_value=9999, min_value=1000, label="Código")
 
 class Forgot(forms.Form):
     email = forms.EmailField(max_length=150)
@@ -27,3 +29,22 @@ class NewCode(forms.Form):
 
 class LogOut(forms.Form):
     None
+
+class Bloodpressure(forms.Form):
+    note = forms.CharField(max_length=150)
+    registered_value = forms.DecimalField()
+
+class Glucose(forms.Form):
+    note = forms.CharField(max_length=150)
+    registered_value = forms.DecimalField()
+    
+class Weight(forms.Form):
+    note = forms.CharField(max_length=150, label="Nota")
+    weight = forms.DecimalField(max_value=999, label="Peso (kg)")
+    height = forms.DecimalField(max_value=999, label="Altura (cm)")
+
+class Vaccines(forms.Form):
+    title = forms.CharField(max_length=150)
+    expected_date = forms.DateField()
+    applied_date = forms.DateTimeField()    
+    
--- techConnectBackend/techConnect/views.py
+++ techConnectBackend/techConnect/views.py
@@ -3,19 +3,23 @@ from datetime import datetime, timedelta
 import random
 from django.shortcuts import render
 from django.core.mail import send_mail
-
-from .models import User, Client, TwoFactorCodes
-from .forms import Forgot, LoginForm, RegisterForm, TwoFactor
+from django.contrib import messages
+from .models import Measurement, User, Client, TwoFactorCodes, Vaccine
+from .forms import Forgot, LoginForm, RegisterForm, TwoFactor, Weight
 from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
+
 
 def generateRandomCode():
     random_number = random.randint(1000, 9999)
     return random_number
 
+
 def validateLogin(request):
     if(len(request.session.get('user_email', '')) < 1):
         return False
     return True
+
 
 def validateLoginTwoFA(request):
     if(len(request.session.get('user_email', '')) < 1):
@@ -23,27 +27,32 @@ def validateLoginTwoFA(request):
     if(request.session.get('twofa', False)):
         return True
 
+
 def index(request):
     if(not validateLoginTwoFA(request)):
         return render(request, 'index.html', {'logged_in': False})
     return render(request, 'index.html', {'logged_in': True})
-    
+
 
 def login(request):
-    if(validateLogin(request) or validateLoginTwoFA(request)):
+    if validateLoginTwoFA(request):
         return HttpResponseRedirect('/')
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if(form.is_valid()):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            foundUser = User.objects.filter(email = email)
+            foundUser = User.objects.filter(email=email)
             if(len(foundUser) < 1):
-                return render(request, 'login.html', {'form': form, 'invalidUserOrPassword': True})
+                form.add_error('email', 'Usuário ou senha inválidos!')
+                form.add_error('password', 'Usuário ou senha inválidos!')
+                return render(request, 'login.html', {'form': form})
             if(foundUser[0].password == password):
-                exp_date = (datetime.utcnow() - timedelta(hours=3)) + timedelta(hours=12)
+                exp_date = (datetime.utcnow() - timedelta(hours=3)
+                            ) + timedelta(hours=12)
                 code = generateRandomCode()
-                twoFA = TwoFactorCodes(code = code, exp_date = exp_date, user = foundUser[0])
+                twoFA = TwoFactorCodes(
+                    code=code, exp_date=exp_date, user=foundUser[0])
                 twoFA.save()
                 send_mail(
                     'Conexão Saúde - Código de confirmação',
@@ -56,34 +65,42 @@ def login(request):
                 request.session['twofa'] = False
                 return HttpResponseRedirect('/twofactor')
             else:
-                return render(request, 'login.html', {'form': form, 'invalidUserOrPassword': True})
+                form.add_error('email', 'Usuário ou senha inválidos!')
+                form.add_error('password', 'Usuário ou senha inválidos!')
+                return render(request, 'login.html', {'form': form})
 
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
+
 def twoFactor(request):
     if(validateLoginTwoFA(request)):
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/user')
     if not validateLogin(request):
         return render(request, 'twofactor.html', {'invalid_code': False, 'logged_in': False})
     if(request.method == 'POST'):
         form = TwoFactor(request.POST)
         if(form.is_valid()):
             code = form.cleaned_data['code']
-            foundUser = User.objects.filter(email = request.session['user_email'])
+            foundUser = User.objects.filter(
+                email=request.session['user_email'])
             date = datetime.utcnow() - timedelta(hours=3)
-            foundCodes = TwoFactorCodes.objects.filter(user = foundUser[0], exp_date__gte = date)
+            foundCodes = TwoFactorCodes.objects.filter(
+                user=foundUser[0], exp_date__gte=date)
             if(len(foundCodes) < 1):
-                return render(request, 'twofactor.html', {'form': form, 'invalid_code': True, 'logged_in': True})
+                form.add_error('code', 'Código inválido.')
+                return render(request, 'twofactor.html', {'form': form, 'logged_in': True})
             for foundCode in foundCodes:
                 if foundCode.code == code:
                     request.session['twofa'] = True
-                    return HttpResponseRedirect('/')
-            return render(request, 'twofactor.html', {'form': form, 'invalid_code': True, 'logged_in': True})
+                    return HttpResponseRedirect('/user')
+            form.add_error('code', 'Código inválido.')
+            return render(request, 'twofactor.html', {'form': form, 'logged_in': True})
     else:
         form = TwoFactor()
-    return render(request, 'twofactor.html', {'form':form, 'invalid_code': False, 'logged_in': True})
+    return render(request, 'twofactor.html', {'form': form, 'logged_in': True})
+
 
 def register(request):
     if request.method == 'POST':
@@ -93,23 +110,36 @@ def register(request):
             email = form.cleaned_data['email']
             date = form.cleaned_data['date']
             password = form.cleaned_data['password']
-            user = User(email = email, password = password)
+            phone = form.cleaned_data['phone']
+            cpf = form.cleaned_data['cpf']
+            foundUserByEmail = User.objects.filter(email=email)
+            foundUserByCpf = Client.objects.filter(cpf=cpf)
+            if(len(foundUserByEmail) > 0):
+                form.add_error('email', 'Email já em uso.')
+                return render(request, 'register.html', {'form': form})
+            elif(len(foundUserByCpf) > 0):
+                form.add_error('cpf', 'CPF já em uso.')
+                return render(request, 'register.html', {'form': form})
+            user = User(email=email, password=password)
             user.save()
-            client = Client(name = name, email = email, date = date, user = user)
+            client = Client(name=name, email=email, date=date,
+                            user=user, phone=phone, cpf=cpf)
             client.save()
             return HttpResponseRedirect('/login')
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
 
+
 def newcode(request):
     if(not validateLogin(request)):
         return HttpResponseRedirect('/login')
     if request.method == 'POST':
-        foundUser = User.objects.filter(email = request.session['user_email'])
-        exp_date = (datetime.utcnow() - timedelta(hours=3)) + timedelta(hours=12)
+        foundUser = User.objects.filter(email=request.session['user_email'])
+        exp_date = (datetime.utcnow() - timedelta(hours=3)) + \
+            timedelta(hours=12)
         code = generateRandomCode()
-        twoFA = TwoFactorCodes(code = code, exp_date = exp_date, user = foundUser[0])
+        twoFA = TwoFactorCodes(code=code, exp_date=exp_date, user=foundUser[0])
         twoFA.save()
         send_mail(
             'Conexão Saúde - Código de confirmação',
@@ -122,11 +152,57 @@ def newcode(request):
     else:
         return HttpResponseRedirect('/login')
 
+
 def logout(request):
     request.session['user_email'] = ''
     return HttpResponseRedirect('/')
 
+
 def user(request):
     if(not validateLoginTwoFA(request)):
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/login')
     return render(request, 'user.html')
+
+
+def bloodpressure(request):
+    pass
+
+
+def weight(request):
+    if(not validateLoginTwoFA(request)):
+        return HttpResponseRedirect('/login')
+    client = Client.objects.filter(email=request.session['user_email'])
+    weight_registers = Measurement.objects.filter(client=client[0])
+    weight_paginator = Paginator(weight_registers, 10)
+    page_number = request.GET.get('page')
+    if(page_number is None):
+        page_number = 1
+    page_obj = weight_paginator.get_page(page_number)
+    page_obj.adjusted_elided_pages = weight_paginator.get_elided_page_range(page_number)
+    if(request.method == 'POST'):
+        form = Weight(request.POST)
+        if(form.is_valid()):
+            note = form.cleaned_data['note']
+            weight = form.cleaned_data['weight']
+            height = form.cleaned_data['height']
+            client = Client.objects.filter(email=request.session['user_email'])
+            measurement = Measurement(note = note, weight = weight, height = height, register_date = datetime.utcnow() - timedelta(hours=3), client = client[0])
+            measurement.save()
+            messages.success(request, 'Registro incluído com sucesso!')
+            return render(request, 'weight.html', {'form': form, 'page_obj': page_obj})
+    form = Weight()
+    return render(request, 'weight.html', {'form': form, 'page_obj': page_obj})
+
+
+def vaccines(request):
+    if(not validateLoginTwoFA(request)):
+        return HttpResponseRedirect('/login')
+    if(request.method == 'POST'):
+        form = Vaccine(request.POST)
+        # if
+
+    return render(request, 'vaccines.html')
+
+
+def glucose(request):
+    pass
